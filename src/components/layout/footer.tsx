bb00ca3c7b35791ba1f5ab5e8eac127af87b80b6
Abbos Nurgulshanov <bustha.rhymes19970410@gmail.com>
2025-01-30 17:40:53 +0500

    button fixed

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,66 +1,65 @@
-import { NAV_LINKS } from "@/constants/global";
-import Link from "next/link";
-import React from "react";
-import NavbarLink from "../ui/nav-link";
+import { NAV_LINKS } from '@/constants/global';
+import Link from 'next/link';
+import React from 'react';
+import NavbarLink from '../ui/nav-link';
 
 const Footer = () => {
 	return (
-		<footer className="pb-[60px] md:pt-[120px] pt-14  bg-black relative">
+		<footer className='pb-[60px] md:pt-[120px] pt-14  bg-black relative'>
 			<video
-				className="object-cover h-full w-full absolute top-0 left-0"
-				src="/videos/footer-video.mp4"
+				className='object-cover h-full w-full absolute top-0 left-0'
+				src='/videos/footer-video.mp4'
 				autoPlay
 				muted
 				loop
 			/>
 			<div
-				className="absolute top-0 left-0 w-full h-full"
+				className='absolute top-0 left-0 w-full h-full'
 				style={{
 					background:
-						"radial-gradient(50% 50% at 50% 50%, rgba(0, 0, 0, 0.00) 0%, #000 100%)",
+						'radial-gradient(50% 50% at 50% 50%, rgba(0, 0, 0, 0.00) 0%, #000 100%)',
 				}}
 			></div>
-			<div className="max-w-[1576px] px-6 mx-auto relative z-[2]">
-				<div className="flex justify-between border-b border-white/10 pb-6 mb-6 max-md:flex-col gap-10">
+			<div className='max-w-[1576px] px-6 mx-auto relative z-[2]'>
+				<div className='flex justify-between border-b border-white/10 pb-6 mb-6 max-md:flex-col gap-10'>
 					<div>
 						<Link
-							href="/"
-							aria-label="Home"
-							className="text-white uppercase text-xl font-bold"
+							href='/'
+							aria-label='Home'
+							className='text-white uppercase text-xl font-bold'
 						>
 							SHOAZIZOV
 						</Link>
-						<span className="text-white/50 text-sm max-w-[400px] block mt-4">
+						<span className='text-white/50 text-sm max-w-[400px] block mt-4'>
 							Experienced interior and exterior designer offering personalized
 							solutions. Create stunning spaces that reflect your unique style.
 							Contact me today for a consultation.
 						</span>
 					</div>
-					<ul className="flex flex-col items-center gap-4 max-md:items-start">
+					<ul className='flex flex-col items-center gap-4 max-md:items-start'>
 						{NAV_LINKS.map(item => (
-							<NavbarLink
-								key={item.link + "footer"}
-								{...item}
-							/>
+							<li key={item.link + 'footer'}>
+								<NavbarLink {...item} />
+							</li>
 						))}
 					</ul>
 				</div>
-				<div className="flex items-center justify-between gap-6 flex-col md:flex-row">
-					<p className="text-white/50 text-sm font-light max-md:text-center">
-						Copyright {new Date().getFullYear()} TIM. Site made by{" "}
+				<div className='flex items-center justify-between gap-6 flex-col md:flex-row'>
+					<p className='text-white/50 text-sm font-light max-md:text-center'>
+						Copyright {new Date().getFullYear()} TIM. Site made by{' '}
 						<Link
-							target="_blank"
-							href="https://abbos-portfolio.vercel.app/"
-							className="font-semibold text-white"
+							target='_blank'
+							href='https://abbos-portfolio.vercel.app/'
+							className='font-semibold text-white'
 						>
 							Abbos Nurgulshanov
 						</Link>
 					</p>
-					<ul className="flex items-center gap-6">
+					<ul className='flex items-center gap-6'>
 						{FOOTER_LINKS.map(item => (
-							<li key={item.href + "footer"}>
+							<li key={item.href + 'footer'}>
 								<Link
-									className="text-sm font-light text-gray-500 dark:text-gray-400 hover:text-white transition-all duration-200 ease-linear"
+									className='text-sm font-light text-gray-500 dark:text-gray-400 hover:text-white transition-all duration-200 ease-linear'
 									href={item.href}
 								>
 									{item.name}
@@ -76,16 +75,16 @@ const Footer = () => {
 
 const FOOTER_LINKS = [
 	{
-		href: "https://www.upwork.com/freelancers/~01511a379fdf491eb9",
-		name: "Upwork",
+		href: 'https://www.upwork.com/freelancers/~01511a379fdf491eb9',
+		name: 'Upwork',
 	},
 	{
-		href: "https://www.instagram.com/shoazizovarchitects",
-		name: "Instagram",
+		href: 'https://www.instagram.com/shoazizovarchitects',
+		name: 'Instagram',
 	},
 	{
-		href: "https://www.behance.net/shahobishoaziz",
-		name: "Behance",
+		href: 'https://www.behance.net/shahobishoaziz',
+		name: 'Behance',
 	},
 ];
 
--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -1,24 +1,24 @@
-"use client";
+'use client';
 import {
 	motion,
 	useScroll,
 	useMotionValueEvent,
 	AnimatePresence,
-} from "framer-motion";
-import React, { useState } from "react";
+} from 'framer-motion';
+import React, { useState } from 'react';
 
-import { NAV_LINKS } from "@/constants/global";
-import { cn } from "@/lib/utils/cn";
-import NavbarLink from "../ui/nav-link";
-import Link from "next/link";
+import { NAV_LINKS } from '@/constants/global';
+import { cn } from '@/lib/utils/cn';
+import NavbarLink from '../ui/nav-link';
+import Link from 'next/link';
 
 const Navbar = () => {
 	const [open, setOpen] = useState(false);
 	const [visible, setVisible] = useState(true);
 	const { scrollYProgress } = useScroll();
-	useMotionValueEvent(scrollYProgress, "change", current => {
+	useMotionValueEvent(scrollYProgress, 'change', current => {
 		// Check if current is not undefined and is a number
-		if (typeof current === "number") {
+		if (typeof current === 'number') {
 			let direction = current! - scrollYProgress.getPrevious()!;
 
 			if (scrollYProgress.get() < 0.05) {
@@ -34,7 +34,7 @@ const Navbar = () => {
 	});
 
 	return (
-		<AnimatePresence mode="wait">
+		<AnimatePresence mode='wait'>
 			<motion.header
 				initial={{
 					opacity: 1,
@@ -46,34 +46,35 @@ const Navbar = () => {
 				transition={{
 					duration: 0.3,
 				}}
-				className="fixed top-5 left-0 ring-0 mx-auto    z-[9999999] flex items-center flex-col justify-center px-6 transition-all duration-200 w-full"
+				className='fixed top-5 left-0 ring-0 mx-auto    z-[9999999] flex items-center flex-col justify-center px-6 transition-all duration-200 w-full'
 			>
-				<div className=" border border-white/10 rounded-lg bg-white/10 backdrop-blur-[200px] py-3 flex items-center w-full max-w-[760px] justify-between px-6">
+				<div className=' border border-white/10 rounded-lg bg-white/10 backdrop-blur-[200px] py-3 flex items-center w-full max-w-[760px] justify-between px-6'>
 					<Link
-						href="/"
-						className="text-white"
+						href='/'
+						className='text-white'
 					>
 						SHOAZIZOV
 					</Link>
-					<nav className="md:flex hidden items-center gap-6">
+					<nav className='md:flex hidden items-center gap-6'>
 						{NAV_LINKS.map(item => (
 							<NavbarLink
-								key={item.link + "comp"}
+								key={item.link + 'comp'}
 								{...item}
 							></NavbarLink>
 						))}
 					</nav>
-					<span className="text-white/60 text-sm md:block hidden">
+					<span className='text-white/60 text-sm md:block hidden'>
 						Tashkent
 					</span>
 					<button
+						aria-label='Plus icon'
 						onClick={() => setOpen(!open)}
-						type="button"
-						className="md:hidden h-8 w-8 flex items-center justify-center rounded border border-white/10"
+						type='button'
+						className='md:hidden h-8 w-8 flex items-center justify-center rounded border border-white/10'
 					>
 						<span
-							className={cn("transition-all duration-200 ease-in-out", {
-								"rotate-45": open,
+							className={cn('transition-all duration-200 ease-in-out', {
+								'rotate-45': open,
 							})}
 						>
 							<PlusSignIcon />
@@ -81,11 +82,11 @@ const Navbar = () => {
 					</button>
 				</div>
 				{open && (
-					<div className="md:hidden border mt-4 border-white/10 rounded-lg bg-white/10 backdrop-blur-[200px] w-full max-w-[760px] px-4 py-6 animate-mobile-menu">
-						<div className="flex flex-col items-start gap-6">
+					<div className='md:hidden border mt-4 border-white/10 rounded-lg bg-white/10 backdrop-blur-[200px] w-full max-w-[760px] px-4 py-6 animate-mobile-menu'>
+						<div className='flex flex-col items-start gap-6'>
 							{NAV_LINKS.map(item => (
 								<NavbarLink
-									key={item.link + "mob"}
+									key={item.link + 'mob'}
 									{...item}
 								></NavbarLink>
 							))}
@@ -99,20 +100,20 @@ const Navbar = () => {
 
 const PlusSignIcon = (props: React.SVGProps<SVGSVGElement>) => (
 	<svg
-		xmlns="http://www.w3.org/2000/svg"
-		viewBox="0 0 24 24"
+		xmlns='http://www.w3.org/2000/svg'
+		viewBox='0 0 24 24'
 		width={20}
 		height={20}
-		color={"#fff"}
-		fill={"none"}
+		color={'#fff'}
+		fill={'none'}
 		{...props}
 	>
 		<path
-			d="M12 4V20M20 12H4"
-			stroke="currentColor"
-			strokeWidth="1.5"
-			strokeLinecap="round"
-			strokeLinejoin="round"
+			d='M12 4V20M20 12H4'
+			stroke='currentColor'
+			strokeWidth='1.5'
+			strokeLinecap='round'
+			strokeLinejoin='round'
 		/>
 	</svg>
 );
